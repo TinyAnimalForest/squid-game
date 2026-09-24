0cbd3aad66302fdf53cfc7df7bc00ef5b05a4860
wunksta <wunksta@gmail.com>
2026-09-23 22:03:29 -0500

    Enemies
    Added enemies that spawn in a random area

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -21,3 +24,28 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
