f8c1e8381db4a2e714b9a993088320a708ff53ba
wunksta <wunksta@gmail.com>
2026-09-24 20:17:35 -0500

    High Score
    Added new high score

--- main.gd
+++ main.gd
@@ -1,31 +1,29 @@
 extends Node
 
 @export var mob_scene: PackedScene
-var score
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+var score: int = 0
+var high_score: int = 0
+var new_high_score: bool
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.update_score(score)
+	$HUD.show_game_over(high_score,new_high_score)
+	get_tree().call_group("mobs", "queue_free")
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	new_high_score = false
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout() -> void:
 		# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
+	mob.add_to_group("mobs")
 
 	# Choose a random location on Path2D.
 	var mob_spawn_location = $MobPath/MobSpawnLocation
@@ -33,7 +31,7 @@ func _on_mob_timer_timeout() -> void:
 
 	# Set the mob's position to the random location.
 	mob.position = mob_spawn_location.position
-
+	
 	# Set the mob's direction perpendicular to the path direction.
 	var direction = mob_spawn_location.rotation + PI / 2
 
@@ -51,8 +49,21 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
-
+	$HUD.update_score(score)
+	if score > high_score:
+		high_score = score
+		new_high_score = true
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#new_game()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
